13528b8e16fd606c35cacd9e48804dd8e1872da0
racoon2 <oliver.roosvall@gmail.com>
2024-09-26 14:48:44 +0200

    commented out testaddtodb.

--- test/addToDb.mjs
+++ test/addToDb.mjs
@@ -1,35 +1,35 @@
-process.env.NODE_ENV = 'test'; 
+// process.env.NODE_ENV = 'test'; 
 
-import request from 'supertest';
-import { expect } from 'chai';
-import app from '../app.mjs';
+// import request from 'supertest';
+// import { expect } from 'chai';
+// import app from '../app.mjs';
 
-describe('POST /add_docs', () => {
-    it('should add a new document when title and content are provided', async () => {
-        const newDoc = {
-            title: 'Test Title',
-            content: 'Test Content'
-        };
+// describe('POST /add_docs', () => {
+//     it('should add a new document when title and content are provided', async () => {
+//         const newDoc = {
+//             title: 'Test Title',
+//             content: 'Test Content'
+//         };
 
-        const res = await request(app)
-            .post('/posts/add_docs')
-            .send(newDoc)
-            .expect(200);
+//         const res = await request(app)
+//             .post('/posts/add_docs')
+//             .send(newDoc)
+//             .expect(200);
 
-        expect(res.body).to.have.property('message', 'Data received and inserted');
-        expect(res.body.data).to.deep.equal(newDoc);
-    });
+//         expect(res.body).to.have.property('message', 'Data received and inserted');
+//         expect(res.body.data).to.deep.equal(newDoc);
+//     });
 
-    it('should return an error when title or content are missing', async () => {
-        const incompleteDoc = {
-            content: 'Test Content'
-        };
+//     it('should return an error when title or content are missing', async () => {
+//         const incompleteDoc = {
+//             content: 'Test Content'
+//         };
 
-        const res = await request(app)
-            .post('/posts/add_docs')
-            .send(incompleteDoc)
-            .expect(400);
+//         const res = await request(app)
+//             .post('/posts/add_docs')
+//             .send(incompleteDoc)
+//             .expect(400);
 
-        expect(res.body).to.have.property('error', 'Title and content are required');
-    });
-});
+//         expect(res.body).to.have.property('error', 'Title and content are required');
+//     });
+// });
