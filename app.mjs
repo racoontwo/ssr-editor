8694b007ffc5df04d6da3b27bdbf5fe54e6aa6cb
racoon2 <oliver.roosvall@gmail.com>
2024-10-07 14:30:48 +0200

    first socket in place, basic structure works.

--- app.mjs
+++ app.mjs
@@ -6,21 +6,12 @@ import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
 import posts from "./routes/posts.mjs";
-import { createServer } from 'http';
+// import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 
 const port = process.env.PORT || 1337;
 const app = express();
-const httpServer = createServer(app);
-
-// Setup Socket.IO
-const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:3000", // Adjust origin as needed
-        methods: ["GET", "POST"]
-    }
-});
 
 app.disable('x-powered-by');
 
@@ -31,6 +22,16 @@ app.use(express.static(path.join(process.cwd(), "public")));
 if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('combined'));
 }
+
+// const httpServer = createServer(app);
+
+// Setup Socket.IO
+// const io = new Server(server, {
+//     cors: {
+//         origin: "http://localhost:3000", // Adjust origin as needed
+//         methods: ["GET", "POST"]
+//     }
+// });
 
 // app.use(cors({
 //     // origin: 'http://localhost:3000'
@@ -49,19 +50,53 @@ app.get('/', (req, res) => {
 
 app.use("/posts", posts);
 
+// io.on('connection', (socket) => {
+//     console.log(`A user connected: ${socket.id}`);
+    
+
+//     socket.on('frontend', (message) => {
+//         let servermessge = "hello from server";
+//         socket.emit("servermsg", servermessge);
+//         console.log(message);
+//     });
+
+//     socket.on('create', function(room) {
+//         socket.join(room);
+//     });
+
+//     // Handle disconnect
+//     socket.on('disconnect', () => {
+//         console.log(`User disconnected: ${socket.id}`);
+//     });
+// });
+
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // Adjust origin as needed
+        methods: ["GET", "POST"]
+    }
+});
+
 io.on('connection', (socket) => {
     console.log(`A user connected: ${socket.id}`);
-    console.log(`what is socket: ${socket}`);
-
-    // Example of emitting a message when connected
-    // socket.emit('message', { data: 'Welcome to the Socket.IO server!' });
-
-    socket.on('message', () => {
-        console.log('this is message!');
+    socket.on('selectedItem', (itemData) => {
+        if (itemData) {
+            console.log("this is the id", itemData._id);
+        }
+        // console.log("This is the itemData", itemData);
     });
 
+    //"room" är detsamma som "item._id". blanda inte ihop med user.id
+    //this is the id 66f3d02a28bec97216e15e4f
+    //this is the room 66f3d02a28bec97216e15e4f
     socket.on('create', function(room) {
-        socket.join(room);
+        console.log("this is the room", room);
+        // socket.join(room);
     });
 
     // Handle disconnect
@@ -70,16 +105,11 @@ io.on('connection', (socket) => {
     });
 });
 
-
-// const server = app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
+// httpServer.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
 // });
 
-httpServer.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// export default { app, httpServer };
 
-export default { app, httpServer };
-
-// export default { app, server }
+export default { app, server }
 // export default app
