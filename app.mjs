c5e283e869156f5b48bea596384e64a872b99179
racoon2 <oliver.roosvall@gmail.com>
2024-10-11 14:32:14 +0200

    cleaned up some commented-out sections.

--- app.mjs
+++ app.mjs
@@ -6,9 +6,7 @@ import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
 import posts from "./routes/posts.mjs";
-// import { createServer } from 'http';
 import { Server } from 'socket.io';
-// import { instrument } from '@socket.io/admin-ui';
 
 const port = process.env.PORT || 1337;
 const app = express();
@@ -23,7 +21,6 @@ if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('combined'));
 }
 
-
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -36,7 +33,6 @@ app.get('/', (req, res) => {
 
 app.use("/posts", posts);
 
-
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
@@ -45,9 +41,9 @@ const server = app.listen(port, () => {
 const io = new Server(server, {
     cors: {
         // origin: "http://localhost:3000",
-        origin: "https://www.student.bth.se/~olrs23/editor/",
-        // origin: ["http://localhost:3000", "http://admin.socket.io/"],
-        // methods: ["GET", "POST"]
+        origin: "*",
+        // origin: "https://www.student.bth.se/~olrs23/editor/",
+        methods: ["GET", "POST"]
     }
 });
 
@@ -59,29 +55,18 @@ io.on('connection', (socket) => {
     socket.on('selectedItem', (itemData) => {
         if (itemData) {
             const roomId = itemData._id;
+
             socket.join(roomId)
             console.log(`User ${socket.id} joined room: ${roomId}`);
-
-            // console.log("this is the id", itemData._id);
-            // socket.to(itemData._id).emit("doc", itemData);
         }
-        // console.log("This is the itemData", itemData);
-    });
-
-    //"room" är detsamma som "item._id". blanda inte ihop med user.id
-    //this is the id 66f3d02a28bec97216e15e4f
-    //this is the room 66f3d02a28bec97216e15e4f
-    socket.on('create', function(room) {
-        console.log("this is the room", room);
-        // socket.join(room);
     });
 
     //Handles any changes in a document so that it is immediately reflected for all clients in the room.
     socket.on("doc", (data) => {
         if (data) {
             const roomId = data._id;
+
             io.to(roomId).emit('doc', data);
-            // socket.to(data._id).emit("doc", data);
         }
     });
 
@@ -91,8 +76,4 @@ io.on('connection', (socket) => {
     });
 });
 
-
-// instrument(io, { auth: false});
-
 export default { app, server }
-// export default app
